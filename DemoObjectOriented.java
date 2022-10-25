5c534f655b432e7e281c947de67e39f1499cdeec
dhilla-0 <115519691+dhilla-0@users.noreply.github.com>
2022-10-25 21:41:06 +0700

    Add files via upload

---
+++ DemoObjectOriented.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author FADHILAH KH 21103100
+ */
+public class DemoObjectOriented {
+    public void hello_world (int jmlh_looping){
+        System.out.println ("Display hello world");
+        for(int i=0; i<jmlh_looping; i++){
+            System.out.println(+(i+1)+"hello world");
+        }
+    }
+    public int tambah(int bil1, int bil2){
+        return (bil1+bil2);
+    }
+}
